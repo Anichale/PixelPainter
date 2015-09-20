24a032e870ed9d4c76c9e6530d890c334759f637
Alexander Anich <alexander.anich@gmail.com>
2015-09-19 16:09:59 -1000

    implemented PixelPainter functionality

--- js/mainGrid.js
+++ js/mainGrid.js
@@ -1,12 +1,29 @@
 //setup blank canvas with changeable pixels
+
+/*width = columns, height = rows
+matrix of rows * columns
+populate each cell in matrix with a pixel
+append each pixel to a unique row
+append row to the canvas
+*/
 function PixelPainter (width, height) {
-  /*width = columns, height = rows
-  matrix of rows * columns
-  populate each cell in matrix with a pixel
-  append each pixel to a unique row
-  append row to the canvas
-  */
   var rows = height;
   var columns = width;
-  var
+  var gridPx;
+  var row;
+  var mainGrid = document.createElement('table');
+  mainGrid.id = 'mainGrid';
+
+  for (var i = 0; i < rows; i++) {
+    row = document.createElement('tr');
+    row.className = 'row';
+    for (var j = 0; j < columns; j++) {
+      gridPx = pixelFactory();
+      row.appendChild(gridPx);
+    }
+    mainGrid.appendChild(row);
+  }
+  document.body.appendChild(mainGrid);
 }
+
+PixelPainter(15, 15);
--- js/pixelFactory.js
+++ js/pixelFactory.js
@@ -1,8 +1,3 @@
-//set 'this' pixel color to palette color 'purple' or selectedColor
-function changeColor () {
-
-}
-
 //create a div pixel when invoked
 //pixel will have a background color white
 //width and height property
@@ -11,12 +6,11 @@ function pixelFactory () {
   var returnPx = document.createElement('td');
   returnPx.style.width = '40px';
   returnPx.style.height = '40px';
-  returnPx.style.color = '#FFFFFF';
+  returnPx.style.color = '#000000';
   returnPx.addEventListener('click', changeColor);
   return returnPx;
 }
 
 function changeColor () {
-  this.style.color = selectedColor;
-  console.log(this.style.color);
+  this.style.background = selectedColor;
 }
