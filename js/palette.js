12110f96fe59b8171e087622cd773ff856e1feb4
Alexander Anich <alexander.anich@gmail.com>
2015-09-21 20:38:29 -1000

    start work on colorkey for save

--- js/palette.js
+++ js/palette.js
@@ -7,7 +7,9 @@ PixelPainter.Options = (function () {
   var selectedColor = '#000000';
 
   //swatch values for the palette
-  var swatchArray = ['#CD4A4A','#CC6666','#BC5D58','#FD5E53','#FF7538','#9F8170','#FAA76C','#FF8D88','#FFCF48','#FCE883','#BAB86C','#FDFC74','#87A96B','#1DF914','#76FF7A','#6DAE81','#1CAC78','#178086', '#FFFFFF', '#CFCFCF', '#ABABAB', '#838485', '#494A4A', '#000000', '#CB4154', '#FF9BAA', '#EF98AA', '#EE204D', '#EE204D', '#FC89AC', '#FF1DCE', '#C364C5', '#9D81BA', '#7366BD', '#5D76CB', '#1F75FE'];
+  var swatchArray = ['#CD4A4A','#CC6666','#BC5D58','#FD5E53','#FF7538','#9F8170','#FAA76C','#FF8D88','#FFCF48','#FCE883','#BAB86C','#FDFC74','#87A96B','#1DF914','#76FF7A','#6DAE81','#1CAC78','#178086', '#ADFF2F', '#32CD32', '#20B2AA', '#00FFFF', '#E0FFFF', '#00CED1', '#48D1CC', '#AFEEEE', '#4682B4', '#000080', '#00008B', '#4169E1', '#8A2BE2', '#4B0082', '#9400D3', '#DDA0DD', '#DA70D6', '#FF69B4', '#FFB6C1', '#FAEBD7', '#F5F5DC', '#FFEBCD', '#FAFAD2', '#8B4513', '#A0522D', '#D2691E', '#FFE4E1', '#FFF0F5', '#FAF0E6', '#FFEFD5', '#E6E6FA', '#F8F8FF', '#F0FFF0', '#FFFFF0','#FFFFFF','#CFCFCF','#ABABAB','#838485','#494A4A','#000000','#CB4154','#FF9BAA','#EF98AA','#EE204D','#EE204D','#FC89AC','#FF1DCE','#C364C5','#9D81BA','#7366BD','#5D76CB','#1F75FE'];
+
+  var colorKey;
 
   /*  creates a color palette with a fixed amount of
   *   swatches created with our createPaletteSwatch()
@@ -15,8 +17,8 @@ PixelPainter.Options = (function () {
   */
   (function createPaletteGrid () {
     //init variables for later changes in matrix
-    var rows = 6;
-    var col = 6;
+    var rows = 12;
+    var col = 5;
     var arrayPos = 0;
     var swatch;
     var paletteRow;
